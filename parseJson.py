0e487b6b3a592ad6865caf2ca59313f07e189cdb
aabdeljamil <43014818+aabdeljamil@users.noreply.github.com>
2021-03-03 00:58:51 -0600

    Update parseJson.py

--- parseJson.py
+++ parseJson.py
@@ -3,17 +3,18 @@ import json
 with open('s2021.json') as f:
     data = json.load(f)
 
-finalDict = {} #contains the 
+finalDict = {} #contains all rooms and their occupied times
 allRooms = []
 lst = []
-for clas in data:#clas is a dictionary
+
+for clas in data:#'clas' is a dictionary
     if clas["is_offered"] == True:
-        for section in clas["semesters"][0]['sections']:#'section is a list
+        for section in clas["semesters"][0]['sections']:#'section' is a list
             for meeting in section['meetings']:#'meeting' is a dictionary
                 if not meeting['where'] == 'TBA': 
                     room = meeting['where'] #room name in the format of 'building name room number"
 
-                    allRooms.append(room) #just for reference idk bruv
+                    allRooms.append(room) #could be useful
 
                     lst = []
                     lst.append(meeting['days'])
