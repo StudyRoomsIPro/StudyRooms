86965ebb171f80eedf036e715c9e4869a3d15047
Abdallah Abdeljamil <aabdeljamil@hawk.iit.edu>
2021-03-11 17:44:28 -0600

    Updated to include campus name, and split the building name from the room number

--- parseJson.py
+++ parseJson.py
@@ -5,11 +5,11 @@ with open('s2021.json') as f:
 
 finalDict = {} #contains all rooms and their occupied times
 allRooms = []
-lst = []
 
 for clas in data:#'clas' is a dictionary
     if clas["is_offered"] == True:
         for section in clas["semesters"][0]['sections']:#'section' is a list
+            campus = section['campus']
             for meeting in section['meetings']:#'meeting' is a dictionary
                 if not meeting['where'] == 'TBA': 
                     room = meeting['where'] #room name in the format of 'building name room number"
@@ -21,9 +21,19 @@ for clas in data:#'clas' is a dictionary
                     lst.append(meeting['time'])
 
                     if room in finalDict.keys():
-                        finalDict[room].append(lst)
+                        finalDict[room]['scheduled'].append(lst)
                     else:
-                        finalDict[room] = [lst]
+                        finalDict[room] = {'scheduled': [lst]}
+
+                    finalDict[room]['campus'] = campus
+            
+for key in finalDict.keys():
+    temp = key.rsplit(' ', 1)
+    building = temp[0]
+    roomNumber = temp[1]
+    finalDict[key]['building'] = building
+    finalDict[key]['room_number'] = roomNumber
+
                     
 with open('roomInfo.json', 'w') as json_file:
     json.dump(finalDict, json_file, indent=2)
