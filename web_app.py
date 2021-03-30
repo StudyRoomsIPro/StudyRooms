53b7c09df409b0e6b7088b0ed74f0d562a8b2813
Maksym Melnyk <43916250+kpuiiuiii@users.noreply.github.com>
2021-03-30 11:04:48 -0500

    UI
    Add login.html
    Add profile.html
    Add 2 users
    users.append(User(id=1, username='max@hawk.iit.edu', password='123'))
    users.append(User(id=2, username='bob@hawk.iit.edu', password='321'))
    
    Add a user profile session
    Minor UI fixes
    
    What is not working:
    Navbar menu redirection
    If you login as a random user gives an error but if login as a user listed above using a wrong password refreshes the page

--- web_app.py
+++ web_app.py
@@ -1,14 +1,84 @@
-from flask import Flask, render_template, url_for
-app= Flask(__name__)
+from flask import (
+    Flask,
+    g,
+    render_template,
+    url_for, 
+    flash, 
+    redirect, 
+    request, 
+    session, 
+    abort
+)
 
-# this file is used by devs for local developement
-# to run simply python3 web_app.py and then in your browser go to localhost:5000
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+        
+    def __repr(self):
+        return f'<User: {self.username}>'
+    
+
+users = []
+users.append(User(id=1, username='max@hawk.iit.edu', password='123'))
+users.append(User(id=2, username='bob@hawk.iit.edu', password='321'))
+
+app = Flask(__name__)
+app.secret_key = 'asd'
+
+@app.before_request
+def before_request():
+    if 'user_id' in session:
+        user =[x for x in users if x.id == session['user_id']][0]
+        g.user = user
+    else:
+        g.user = None
+
+@app.route('/login', methods=['GET', 'POST'] )
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+        
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('profile'))
+            
+        return redirect(url_for('login'))
+            
+        
+    return render_template('login.html')
+
+
+@app.route('/profile')
+def profile():
+    if not g.user:
+        return redirect(url_for('login'))
+    return render_template('profile.html')
 
 
 if __name__ == "__main__":
     app.static_folder='static'
     app.run(debug=True)
+
+
+
+# from flask import Flask, render_template, url_for, flash, redirect, request, session, abort
+# import os
+# app = Flask(__name__)
+
+# # this file is used by devs for local developement
+# # to run simply python3 web_app.py and then in your browser go to localhost:5000
+
+# @app.route("/")
+# def home():
+#     return render_template('login.html')
+
+# if __name__ == "__main__":
+#     app.static_folder='static'
+#     app.run(debug=True)
