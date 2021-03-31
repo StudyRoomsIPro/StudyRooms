54219c278be6760db8a4ffd4370c64f7ba47fa93
Sreimer78 <sreimer@hawk.iit.edu>
2021-03-30 19:49:49 -0500

    cleaning up code

--- web_app.py
+++ web_app.py
@@ -45,21 +45,9 @@ def profile():
     if not g.user:
         return redirect(url_for('login'))
     return render_template('profile.html')
+@app.route("/")
+def home():
+    return render_template('index.html')
 if __name__ == "__main__":
     app.static_folder='static'
     app.run(debug=True)
-
-# from flask import Flask, render_template, url_for, flash, redirect, request, session, abort
-# import os
-# app = Flask(__name__)
-
-# # this file is used by devs for local developement
-# # to run simply python3 web_app.py and then in your browser go to localhost:5000
-
-# @app.route("/")
-# def home():
-#     return render_template('login.html')
-
-# if __name__ == "__main__":
-#     app.static_folder='static'
-#     app.run(debug=True)
