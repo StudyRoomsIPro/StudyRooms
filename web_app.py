db7f8c72c7cd7c295cbd037adec10c273c03f909
Maksym Melnyk <43916250+kpuiiuiii@users.noreply.github.com>
2021-04-20 18:54:44 -0500

    Change on UI

--- web_app.py
+++ web_app.py
@@ -71,6 +71,7 @@ def contact():
         return redirect(url_for('contact'))
     return render_template('contact.html')
 
+
 if __name__ == "__main__":
     app.static_folder='static'
     app.run(debug=True)
